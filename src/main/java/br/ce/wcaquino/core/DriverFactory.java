552f173f9d7f5af3afd355c30d4b59bcc1e78b03
Alana Mendes <72421437+asm-alana@users.noreply.github.com>
2021-09-06 19:15:12 -0300

    Update DriverFactory.java

--- src/main/java/br/ce/wcaquino/core/DriverFactory.java
+++ src/main/java/br/ce/wcaquino/core/DriverFactory.java
@@ -4,13 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-/*
- * Classe DriverFactory é para configurar os métodos usados para gerenciar o driver
- * Os métodos dessa classe são utilizados em outras através dos métodos quando se dá um import
- *dessa classe.
- * Os métodos não são instanciados nas outras classes porque são estáticos.
- */
-
 public class DriverFactory {
 
 	private static WebDriver driver;
@@ -28,7 +21,7 @@ public class DriverFactory {
 				driver = new ChromeDriver();
 				break;
 			}
-			driver.manage().window().maximize(); // método pra iniciar um driver que ainda não está instanciado
+			driver.manage().window().maximize();
 		}
 		return driver;
 	}
@@ -36,7 +29,7 @@ public class DriverFactory {
 	public static void killDriver() {
 		if (driver != null) {
 			driver.quit();
-			driver = null; // método pra matar um driver sem o NullPointer
+			driver = null; 
 		}
 	}
 }
